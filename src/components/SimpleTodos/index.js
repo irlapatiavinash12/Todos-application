56f289fa22f315dfaf2400f26180fd99fe4fb948
irlapatiavinash12 <irlapati.avinash12@gmail.com>
2024-10-02 10:28:43 +0000

    enhancement

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,3 +1,5 @@
+import {v4 as uuidv4} from 'uuid'
+
 import {Component} from 'react'
 
 import './index.css'
@@ -42,7 +44,7 @@ const initialTodosList = [
 // Write your code here
 
 class SimpleTodos extends Component {
-  state = {todosList: initialTodosList}
+  state = {todosList: initialTodosList, userInput: '', noOfRepeats: 0}
 
   deleteTodo = id => {
     const {todosList} = this.state
@@ -50,12 +52,52 @@ class SimpleTodos extends Component {
     this.setState({todosList: afterDeletionList})
   }
 
+  onChangeInput = event => {
+    this.setState({userInput: event.target.value})
+  }
+
+  onAdd = () => {
+    const {userInput} = this.state
+
+    const newItem = {
+      id: uuidv4(),
+      title: userInput,
+    }
+
+    this.setState(prevState => ({
+      todosList: [...prevState.todosList, newItem],
+      userInput: '',
+    }))
+  }
+
+  onChangeRepeat = event => {
+    this.setState({noOfRepeats: event.target.value})
+  }
+
   render() {
-    const {todosList} = this.state
+    const {todosList, userInput, noOfRepeats} = this.state
     return (
       <div className="main-bg">
         <div className="content-container">
           <h1 className="heading-styling">Simple Todos</h1>
+          <div className="input-container">
+            <input
+              type="text"
+              className="input-field"
+              placeholder="Enter the text"
+              onChange={this.onChangeInput}
+              value={userInput}
+            />
+            <input
+              type="number"
+              value={noOfRepeats}
+              className="repeat-input-field"
+              onChange={this.onChangeRepeat}
+            />
+            <button type="button" className="add-button" onClick={this.onAdd}>
+              Add
+            </button>
+          </div>
           <ul className="unordered-styling">
             {todosList.map(eachitem => (
               <SimpleTodoItem
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,19 +1,48 @@
 // Write your code here
 
+import {useState} from 'react'
+
 import './index.css'
 
 const TodoItem = props => {
   const {itemDetails, deleteTodo} = props
   const {id, title} = itemDetails
+  const [edit, setEdit] = useState(false)
+  const [userEntry, setEntry] = useState(title)
+
   const onDelete = () => {
     deleteTodo(id)
   }
+
+  const onEdit = () => {
+    setEdit(prevEdit => !prevEdit)
+  }
+
+  const onChangeTitle = event => {
+    setEntry(event.target.value)
+  }
+
   return (
     <li className="list-item-styling">
-      <p className="paragraph-styling">{title}</p>
-      <button className="button-styling" type="button" onClick={onDelete}>
-        Delete
-      </button>
+      <input type="checkbox" />
+      {edit ? (
+        <input
+          className="title-input"
+          onChange={onChangeTitle}
+          value={userEntry}
+        />
+      ) : (
+        <p className="paragraph-styling">{userEntry}</p>
+      )}
+
+      <div>
+        <button type="button" className="edit-button" onClick={onEdit}>
+          {edit ? 'Save' : 'Edit'}
+        </button>
+        <button className="button-styling" type="button" onClick={onDelete}>
+          Delete
+        </button>
+      </div>
     </li>
   )
 }
